1c76a1437d2a35f9fd4347d66cbf5fa8cf1367c9
Thomas Frey <thomas.frey@code.berlin>
2022-11-04 21:20:22 +0100

    add model for daily scrum

--- Scrumdigger/Models/DailyScrum.swift
+++ Scrumdigger/Models/DailyScrum.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+//structure with properties
+struct DailyScrum {
+    var title: String
+    var attendees: [String]
+    var lengthInMinutes: Int
+    var theme: Theme
+}
+
+//extension with sample data
+extension DailyScrum {
+    static let sampleData: [DailyScrum] =
+    [
+        DailyScrum(title: "Design", attendees: ["Thomas", "Liam", "Oliver"], lengthInMinutes: 20, theme: .yellow),
+        DailyScrum(title: "Product Manage", attendees: ["Thomas", "Johanna", "Tina", "Yannix"], lengthInMinutes: 10, theme: .orange),
+        DailyScrum(title: "Software", attendees: ["Santa Clause", "Jerry from Space", "Emily from TV"], lengthInMinutes: 3, theme: .poppy)
+    ]
+}
