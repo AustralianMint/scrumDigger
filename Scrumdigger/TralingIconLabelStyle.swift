740e3f31151ea675c9cbe7e78d1f2440a38eeff4
Thomas Frey <thomas.frey@code.berlin>
2022-11-04 21:24:54 +0100

    add custom label style preset

--- Scrumdigger/TralingIconLabelStyle.swift
+++ Scrumdigger/TralingIconLabelStyle.swift
@@ -5,4 +5,18 @@
 //  Created by Thomas Frey on 04.11.22.
 //
 
-import Foundation
+import SwiftUI
+
+//custom label style for all labels
+struct TrailingIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self() }
+}
